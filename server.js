1dab2b462738b085bc14940928fedbe6ae052f64
Cristina Gutierrez <cristinagutierrez@Cristinas-MBP.lan>
2023-06-26 20:48:07 -0500

    got server to connect

--- server.js
+++ server.js
@@ -1,20 +1,50 @@
-const express = require("express");
-const app = express();
-const PORT = 8080;
+const express= require ("express");
 
-const morgan = require("morgan");
-app.use(morgan("dev"));
+const app= express ();
 
-const cors = require("cors");
-app.use(cors());
+function myFirstMiddleware(req, res, next) {
+  console.log("We have received a request")
+  console.log("Now we will respond")
+  next(); 
+}
+app.use(myFirstMiddleware)
 
-app.use(express.json())
+app.use(express.json());
 
-const client = require('./db/client')
-client.connect()
+async function checkIfTokenExists(req, res, next) {
+      try {
+          console.log(req.headers)
+  
+          if (req.headers.Authorization.length) {
+              console.log("User is valid");
+          } else {
+              console.log("Error. User is not valid.");
+          }
+      } catch (error) {
+          console.log(error); 
+      }
+  }
+  app.use(checkIfTokenExists)
 
-app.use('/api', require('./api'))
+const {fetchTripById, fetchAllTrips, createNewTrip} =  require("./db/seed");
 
-app.listen(PORT, () => {
-  console.log(`The server is up and running on port: ${PORT}`);
-});
+async function getAllTrips(req, res, next){
+  try{
+  const actualTripData = await fetchAllTrips();
+  if(actualTripData.length){
+      res.send(actualTripData)
+  }else{
+      res.send("no trips rendered..")
+  }
+  }catch(error){
+      console.error(error)
+  }
+}
+app.get("/trips1", getAllTrips)
+
+const client= require ("./db/index")
+client.connect ();
+
+app.listen (3000, () => {
+  console.log ("We are now connected to port 3000.")
+})
